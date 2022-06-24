8fbf03b9ca361f03cbfcfd96d29c897869b00450
MVH <MVH@gmail.com>
2022-06-24 09:41:36 +0000

    parameterising container port

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ dockerhub_username=matvh15
 app_name=lbg-api-app
 tag=latest
 image_name=$dockerhub_username/$app_name:$tag
+container_port=5000
 
 echo "Building image:"
 docker build -t $image_name .
@@ -15,4 +16,4 @@ docker stop $app_name
 docker rm $app_name
 
 echo "Running container"
-docker run -d -p 80:8080 --name $app_name $image_name
+docker run -d -p 80:$container_port --name $app_name --env PORT=$container_port $image_name
